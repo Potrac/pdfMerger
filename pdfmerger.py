de949ceec3c7fb79b4d9c5ed52362aa21201878a
Potrac <olivier.grognuz@gmail.com>
2019-08-12 15:55:51 +0200

    added -o flag

--- pdfmerger.py
+++ pdfmerger.py
@@ -3,17 +3,20 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Merge pdfs')
-parser.add_argument('path', metavar='paths', nargs='+',
-                    help='merge pdfs')
+parser.add_argument('path', nargs = '+',
+                    help = 'merge pdfs')
+parser.add_argument('-o', default = '',
+                    help = 'output path (directory), default is current directory')
 args = parser.parse_args()
 
 pdf_paths = args.path
+output_path = args.o
 
 def pdfMerger(pdfs):
     merger = PdfFileMerger()
     for pdf in pdfs:
         merger.append(pdf, bookmark = 'End of file') 
-    with open("C:/Users/Olivier/Desktop/pdf-merged.pdf", 'wb') as output:
+    with open(f"{output_path}pdf-merged.pdf", 'wb') as output:
         merger.write(output)
     return True
 
